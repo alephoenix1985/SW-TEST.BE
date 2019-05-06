07146e6ae6e5c494c60616c1eb6bb50ddcdddfb6
Alejandro Hernandez <alephoenix1985@gmail.com>
2019-05-06 19:36:34 -0300

    fixing deploy issue with stages

--- config/dynamodb.js
+++ config/dynamodb.js
@@ -3,15 +3,11 @@
 const AWS = require('aws-sdk/index'); // eslint-disable-line import/no-extraneous-dependencies
 
 let options = {};
-const env = process.env.STAGE === 'dev' || !process.env.STAGE ? 'local' : process.env.STAGE;
+const env = process.env.STAGE;
 if (!AWS.config.credentials.accessKeyId) {
     options = require('./dynamodb.json')[env];
-} else if (env === 'local' || env === 'dev') {// connect to local DB if running offline
-    options = {
-        region: 'localhost',
-        endpoint: 'http://localhost:8000',
-    }
 }
-
-const client = new AWS.DynamoDB.DocumentClient(options);
+const client = new AWS.DynamoDB.DocumentClient(
+    process.env.IS_OFFLINE ? {region: 'localhost', endpoint: 'http://localhost:8000'} : options
+);
 module.exports = client;
